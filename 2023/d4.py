71ab0931c9c36207327e0b91f5d55b6123656902
asaadme <mehran.asaad@gmail.com>
2023-12-04 10:32:19 +0330

    add recursive method for day4 part2

--- 2023/d4.py
+++ 2023/d4.py
@@ -17,7 +17,7 @@ for card in inp:
     
 print('Part1: ',ans1)
 
-
+#Part2: using stack
 ans2 = 0
 cards_stack = list(range(1,len(inp)+1))
 while cards_stack:
@@ -25,5 +25,14 @@ while cards_stack:
     ans2 += 1
     if matches[cur_card-1]:
         cards_stack.extend([cur_card+a for a in range(1,matches[cur_card-1]+1)])
+
+#Part2: using recursive method
+def rec(l):
+    if len(l) == 1:
+        if matches[l[0]-1]:
+            return 1+rec([l[0]+a for a in range(1,matches[l[0]-1]+1)])
+        else:
+            return 1
+    return rec(l[:-1])+rec(l[-1:])
     
 print('Part2: ', ans2)
